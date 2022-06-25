31d2f87e82f1cff337c7679261e6d67d402c79be
MiyyahCodes <adebimpeadefila64@gmail.com>
2022-06-25 16:53:29 +0100

    all files pushed

--- I4G000488INY/blog/views.py
+++ I4G000488INY/blog/views.py
@@ -7,24 +7,24 @@ from .models import Post
 
 # Create your views here.
 class PostListView(ListView):
-
+ 
     # specify the model for list view
     model = Post
 
-    class PostCreateView(CreateView):
-        model = Post
-        fields = "__all__"
-        success_url = reverse_lazy("blog:all")
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
 
-    class PostDetailView(DetailView):
-        model = Post
+class PostDetailView(DetailView):
+    model = Post
 
-    class PostUpdateView(UpdateView):
-        model = Post
-        fields = "__all__"
-        success_url = reverse_lazy("blog:all")
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
 
-    class PostDeleteView(UpdateView):
-        model = Post
-        fields = "__all__"
-        success_url = reverse_lazy("blog:all")
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
